405333716e1523746e4813c53f0d83b68422b2ad
Muhammad Aijaz <muhammadaijaz76@gmail.com>
2021-04-27 13:52:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> hello pipeline </p>
+        <p> hello pipeline 123</p>
       </header>
     </div>
   );
