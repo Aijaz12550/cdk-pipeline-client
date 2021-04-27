a0a585391b9a4c78bd89a62805b2f26dac6522e4
Muhammad Aijaz <muhammadaijaz76@gmail.com>
2021-04-27 13:25:52 +0500

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>master</h1>
+        <h1>master 123 </h1>
       </header>
     </div>
   );
