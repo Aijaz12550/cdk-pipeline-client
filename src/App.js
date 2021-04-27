154cd1a9eb38827e25c5f9c29897e12ac89058ea
Muhammad Aijaz <muhammadaijaz76@gmail.com>
2021-04-27 14:30:43 +0500

    bb

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> hello pipeline aaaaaaaaa</p>
+        <p> hello pipeline bbbb  bbbbb</p>
       </header>
     </div>
   );
